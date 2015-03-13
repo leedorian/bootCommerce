85e5d6e4337b5a1882204f25fdc47254bc71fda9
Dorian <leedorian@gmail.com>
2015-03-13 15:25:11 +0800

    html hint

--- Gruntfile.js
+++ Gruntfile.js
@@ -419,7 +419,7 @@ module.exports = function (grunt) {
     // CSS distribution task.
     grunt.registerTask('less-compile', ['less:compileBlock', 'less:compilePage']);
     grunt.registerTask('dist-css', ['less-compile', 'autoprefixer:blocks', 'autoprefixer:pages', 'csscomb:blocks', 'csscomb:pages', 'csslint:blocks', 'csslint:pages', 'cssmin:minifyBlocks', 'usebanner']);
-    grunt.registerTask('dist-pages', ['processhtml:blocks','processhtml:pages', 'htmlhintplus']);
+    grunt.registerTask('dist-pages', ['htmlhintplus', 'processhtml:blocks','processhtml:pages']);
 
     // Full distribution task.
     grunt.registerTask('dist', ['clean', 'dist-css', 'dist-pages', 'dist-js']);
