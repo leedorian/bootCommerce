7cb25ab3ed6e7c85e3783515eb31fd49c1452d1e
Dorian <leedorian@gmail.com>
2015-03-18 15:44:24 +0800

    add handlebars

--- blocks/js/bootCommerceBase.js
+++ blocks/js/bootCommerceBase.js
@@ -3,3 +3,24 @@
  * @author Dorian
  */
 var bootCommerce = bootCommerce || {};
+
+/**
+ * bootCommerce untilities
+ */
+bootCommerce.Utils = {
+    /**
+     * compile handlebars template
+     * @param {string} tempID the id selector of template script
+     * @param {object} data the context data to compile the template
+     * @return {string} the compiled html
+     */
+    template : function (tempID,data) {
+        var source = $(tempID).html();
+        var template = Handlebars.compile(source);
+        var context ={};
+        if(data && typeof data === "object"){
+            context = data;
+        }
+        return template(context);
+    }
+}
