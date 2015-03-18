c593487e18bc86474b347f20565ec86f74df68dd
Dorian <leedorian@gmail.com>
2015-03-18 11:52:09 +0800

    index page

--- pages/js/home.js
+++ pages/js/home.js
@@ -11,6 +11,16 @@
             // load default fragment
             this.bindEvent();
         },
+        toggleSideBar: function (beacon) {
+            if(!beacon){
+                jQuery(".masthead").removeClass("expanded");
+                jQuery(".masthead .fa").removeClass("fa-angle-double-left").addClass("fa-angle-double-right");
+            }else{
+                jQuery(".masthead").addClass("expanded");
+                jQuery(".masthead .fa").removeClass("fa-angle-double-right").addClass("fa-angle-double-left");
+            }
+
+        },
         bindEvent: function () {
             var that = this;
             jQuery('.masthead-nav.blocks').on('click', 'a', function (event) {
@@ -20,15 +30,21 @@
             //side bar
             jQuery("body").on("click", function (e) {
                 if (jQuery(e.target).parents(".masthead").size() === 0) {
-                    jQuery(".masthead").removeClass("expanded");
+                    that.toggleSideBar();
                 }else{
-                    jQuery(".masthead").addClass("expanded");
+                    if(jQuery(e.target).hasClass("fa")){
+                        if(jQuery(".masthead").hasClass("expanded")){
+                            that.toggleSideBar();
+                        }else{
+                            that.toggleSideBar(true);
+                        }
+                    }
                 }
 
             });
-            jQuery(".masthead").on("mouseleave",function () {
+            /*jQuery(".masthead").on("mouseleave",function () {
                 jQuery(".masthead").removeClass("expanded");
-            });
+            });*/
         },
         loadHtmlFragment: function (module) {
             // dufault load from blocks/***
