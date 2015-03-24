c9aa971fa3be023573086d344b05e93aa482410b
Lionel Dong <1950195@qq.com>
2015-03-24 23:00:53 +0800

    update image viewer

--- blocks/js/bootCommerce.ImageViewer.js
+++ blocks/js/bootCommerce.ImageViewer.js
@@ -13,5 +13,36 @@ bootCommerce.ImageViewer = function(conf) {
         $(this).closest(".imageViewer")
             .find(".imageViewerBooth img")
             .attr("src", ($(this).data("img-booth")));
+    }).on("mousemove.ImageViewer", ".imageViewerBooth", function(e) {
+        var boothOffset = $(this).offset();
+        var lensOffset = {};
+        var $imageViewerLens = $(this).find(".imageViewerLens");
+        var $imageViewerOverlayImg = $(this).find(".imageViewerOverlay img");
+        var boothWidth = $(this).width();
+        var boothHeight = $(this).height();
+        var lensWidth = $imageViewerLens.width();
+        var lensHeight = $imageViewerLens.height();
+
+        if (e.pageX - boothOffset.left <= lensWidth / 2) {
+            lensOffset.left = 0;
+        } else if (e.pageX - boothOffset.left + lensWidth / 2 >= boothWidth) {
+            lensOffset.left = boothWidth - lensWidth;
+        } else {
+            lensOffset.left = e.pageX - boothOffset.left - lensWidth / 2;
+        }
+
+        if (e.pageY - boothOffset.top <= lensHeight / 2) {
+            lensOffset.top = 0;
+        } else if (e.pageY - boothOffset.top + lensHeight / 2 >= boothHeight) {
+            lensOffset.top = boothHeight - lensHeight;
+        } else {
+            lensOffset.top = e.pageY - boothOffset.top - lensHeight / 2;
+        }
+        $imageViewerLens.css(lensOffset);
+        $imageViewerOverlayImg.css({marginTop: - lensOffset.top * 2, marginLeft: - lensOffset.left * 2});
+    }).on("mouseenter.ImageViewer", ".imageViewerBooth", function(e) {
+        $(this).find(".imageViewerOverlay").show();
+    }).on("mouseleave.ImageViewer", ".imageViewerBooth", function(e) {
+        $(this).find(".imageViewerOverlay").hide();
     });
 };
---
+++ pages/js/home.js
@@ -0,0 +1,102 @@
+/*
+ * describe load html fragment
+ */
+
+/*global jQuery, bootCommerce */
+(function () {
+    'use strict';
+
+    var home = {
+        init: function () {
+            // load default fragment
+            this.bindEvent();
+        },
+        toggleSideBar: function (beacon) {
+            if (!beacon) {
+                jQuery(".masthead").removeClass("expanded");
+                jQuery(".masthead .fa").removeClass("fa-angle-double-left").addClass("fa-angle-double-right");
+            } else {
+                jQuery(".masthead").addClass("expanded");
+                jQuery(".masthead .fa").removeClass("fa-angle-double-right").addClass("fa-angle-double-left");
+            }
+
+        },
+        bindEvent: function () {
+            var that = this;
+            jQuery('.masthead-nav.blocks').on('click', 'a', function (event) {
+                var muduleName = jQuery(event.target).attr('data-page');
+                that.loadHtmlFragment(muduleName);
+            });
+            //side bar
+            jQuery(document).on("click", function (e) {
+                if (jQuery(e.target).parents(".masthead").size() === 0) {
+                    that.toggleSideBar();
+                } else {
+                    if (jQuery(e.target).hasClass("fa")) {
+                        if (jQuery(".masthead").hasClass("expanded")) {
+                            that.toggleSideBar();
+                        } else {
+                            that.toggleSideBar(true);
+                        }
+                    }
+                }
+
+            });
+            /*jQuery(".masthead").on("mouseleave",function () {
+                jQuery(".masthead").removeClass("expanded");
+            });*/
+        },
+        loadHtmlFragment: function (module) {
+            // dufault load from blocks/***
+            jQuery.ajax({
+                type: 'get',
+                url: '../../blocks/dist/' + module,
+                cache: false, // remove cache
+                success: function (result) {
+                    if (result) {
+                        var container = jQuery('.displayContainer');
+                        container.empty().append(result);
+
+
+
+                        /**
+                         * initialize components
+                         */
+
+                        //Product search
+                        var Search = new bootCommerce.Search({
+                            container: ".displayContainer .siteSearchBar",
+                            suggestionCon: ".siteSearchBarAutoSuggest",
+                            keywordInput: ".siteSearchBarInput input[type='text']"
+                        });
+
+                        //Horizontal products carousel
+
+                        //Vertical products carousel
+
+                        //Image viewer
+                        bootCommerce.ImageViewer();
+
+                        //Mini basket
+
+                        //Product swatch
+
+                        //Quick info box
+
+                        //Slide show
+
+                        //Product infos tab
+                        bootCommerce.ProductInfosTab();
+
+                        //
+                    }
+                },
+                error: function () {
+                    // TODO
+                }
+            });
+        }
+    };
+    home.init();
+
+}());
