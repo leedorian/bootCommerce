a856117096f41d1281d04fd308ed5ebdf644f5fe
Jack <79611225@qq.com>
2015-04-03 11:22:55 +0800

    update

--- blocks/js/bootCommerce.Navigation.js
+++ blocks/js/bootCommerce.Navigation.js
@@ -7,21 +7,29 @@
 
 bootCommerce.Search = function (options) {
     "use strict";
-    /* properties */
-    /*$(".mobileHeader").on("click.navigationResponsive", function() {
-        console.log("123");
-    });*/
     var navBarTit = $(".mobileHeader li");
-    navBarTit.each(function() {
-        //console.log($(this));
-        $(this).click(function() {
-            var str = "mobile";
-            var thisClass = $(this).attr("class");
-            if ($(".site" + thisClass.substr(str.length)).length !== 0 ) {
-                console.log($(".site" + thisClass.substr(str.length)));
-                $(".site" + thisClass.substr(str.length)).fadeIn();
-                $(".site" + thisClass.substr(str.length)).addClass("mobileNavActive");
+        navBarTit.click(function() {
+        $("[responsive-mobile-control=true]").hide();
+        //checkHeight();
+        if($(this).hasClass("mobileActive")) {
+            $(this).removeClass("mobileActive");
+        } else {
+            $(".mobileActive").removeClass("mobileActive");
+            $(this).addClass("mobileActive");
+            $("." + $(this).attr("site-target")).show();
+            if (!$(this).hasClass("mobileNavBar")) {
+                //checkHeight($(this).attr("site-target"));
             }
-        });
+        }
     });
+    var checkHeight = function(className) {
+
+        if (className) {
+            $("." + className).height();
+            $(".header").css("margin-bottom", $("." + className).height() + 30);
+            $("." + className).css("padding","15px");
+        } else {
+            $(".header").css("margin-bottom", "0");
+        }
+    }
 };
